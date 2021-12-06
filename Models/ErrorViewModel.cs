c82940e664a8764bbdc070ac5407b2aeea27092f
RIDA <ridaabid25@gmail.com>
2021-12-06 09:21:48 +0000

    Created Signin page and new project

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace TTMS.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
