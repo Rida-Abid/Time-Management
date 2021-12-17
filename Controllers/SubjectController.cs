40742972f1254862782e2e7f71d1a616a57faffa
AMS_FauziaHafiz <hafizfauzia@gmail.com>
2021-12-17 18:56:43 +0000

    TMS-3: Subject list retrieved from database.

--- Controllers/SubjectController.cs
+++ Controllers/SubjectController.cs
@@ -1,12 +1,33 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Dapper;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Data.SqlClient;
+using TTMS.Models;
 
 namespace TTMS.Controllers
 {
     public class SubjectController : Controller
     {
-        public IActionResult Index()
+        private readonly IConfiguration _configuration;
+        private readonly string _connectionString;
+
+        public SubjectController(IConfiguration configuration)
         {
-            return View();
+            _configuration = configuration;
+            _connectionString = _configuration.GetConnectionString("SqlConnection");
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var subjects = await GetSubjectsAsync();
+            return View(subjects);
+        }
+
+        private async Task<List<SubjectViewModel>> GetSubjectsAsync()
+        {
+            var query = "SELECT SubjectID, SubjectName FROM Subject";
+            using var connection = new SqlConnection(_connectionString);
+            var subjects = await connection.QueryAsync<SubjectViewModel>(query);
+            return subjects.ToList();
         }
     }
 }
---
+++ Models/SubjectViewModel.cs
@@ -0,0 +1,10 @@
+﻿namespace TTMS.Models
+{
+    public class SubjectViewModel
+    {
+        public int SubjectID { get; set; }
+
+        public string SubjectName { get; set; }
+
+    }
+}
