954acd52f82614d7b654e95f5cf3405f989f4f24
RIDA <ridaabid25@gmail.com>
2021-12-23 21:01:10 +0500

    AddEdit forms for Teacher, Class, Subject

--- Controllers/SubjectController.cs
+++ Controllers/SubjectController.cs
@@ -1,12 +1,22 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
 
 namespace TTMS.Controllers
 {
     public class SubjectController : Controller
     {
+
+        [Authorize]
         public IActionResult Index()
         {
             return View();
         }
+
+        [Authorize]
+        public IActionResult AddEdit()
+        {
+            return View();
+        }
+
     }
 }
