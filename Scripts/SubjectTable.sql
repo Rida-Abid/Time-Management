a5d54f3b7811795de255bebb2de28e4835997933
Rida-Abid <107047216+Rida-Abid@users.noreply.github.com>
2023-01-25 23:23:00 +0500

    Created 'Subject' table in database.

---
+++ Scripts/SubjectTable.sql
@@ -0,0 +1,18 @@
+
+
+Create table dbo.Subject
+(
+	SubjectID int  NOT NULL PRIMARY KEY IDENTITY(1,1),
+	Name varchar(15) NOT NULL,
+	CreatedDate DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+	UpdatedDate DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP
+);
+
+INSERT INTO dbo.Subject (Name)
+VALUES 
+( 'English'),
+( 'Urdu'),
+( 'Mathematics'),
+( 'Science'),
+( 'Geography')
+ 
