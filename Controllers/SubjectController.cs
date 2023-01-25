f35b87c2656fbd5cfcdc600a8fdceb26e5e55ce8
Rida-Abid <107047216+Rida-Abid@users.noreply.github.com>
2023-01-25 23:24:19 +0500

    Connected databse to Views/subjects/Index

--- Controllers/SubjectController.cs
+++ Controllers/SubjectController.cs
@@ -6,22 +6,47 @@ using TTMS.Models;
 
 namespace TTMS.Controllers
 {
+    //public class SubjectController : Controller
+    //{
+    //    private readonly IConfiguration _configuration;
+    //    private readonly string _connectionString;
+
+    //    public SubjectController(IConfiguration configuration)
+    //    {
+    //        _configuration = configuration;
+    //        _connectionString = _configuration.GetConnectionString("SqlConnection");
+    //    }
+
+    //    [Authorize]
+    //    public async Task<IActionResult> Index()
+    //    {
+    //        var subjects = await GetSubjectsAsync();
+    //        return View(subjects);
+    //    }
+
+    //    [Authorize]
+    //    public IActionResult AddEdit()
+    //    {
+    //        return View();
+    //    }
+
+    //    private async Task<List<SubjectViewModel>> GetSubjectsAsync()
+    //    {
+    //        var query = "SELECT SubjectID, SubjectName FROM Subject";
+    //        using var connection = new SqlConnection(_connectionString);
+    //        var subjects = await connection.QueryAsync<SubjectViewModel>(query);
+    //        return subjects.ToList();
+    //    }
+
+    //}
+
     public class SubjectController : Controller
     {
-        private readonly IConfiguration _configuration;
-        private readonly string _connectionString;
-
-        public SubjectController(IConfiguration configuration)
-        {
-            _configuration = configuration;
-            _connectionString = _configuration.GetConnectionString("SqlConnection");
-        }
-        
         [Authorize]
-        public async Task<IActionResult> Index()
+        public IActionResult Index()
         {
-            var subjects = await GetSubjectsAsync();
-            return View(subjects);
+
+            return View(GetSubjects());
         }
 
         [Authorize]
@@ -29,14 +54,43 @@ namespace TTMS.Controllers
         {
             return View();
         }
-
-        private async Task<List<SubjectViewModel>> GetSubjectsAsync()
+        public List<SubjectRecord> GetSubjects()
         {
-            var query = "SELECT SubjectID, SubjectName FROM Subject";
-            using var connection = new SqlConnection(_connectionString);
-            var subjects = await connection.QueryAsync<SubjectViewModel>(query);
-            return subjects.ToList();
+
+            string connectionString = "Data Source=.\\sqlexpress;Initial Catalog=tms;Integrated Security=True";
+            var listSubjects = new List<SubjectRecord>();
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                connection.Open();
+                string sql = "SELECT * FROM Subject";
+                using (SqlCommand command = new SqlCommand(sql, connection))
+                {
+
+                    using (SqlDataReader reader = command.ExecuteReader())
+                    {
+                        while (reader.Read())
+                        {
+                            SubjectRecord subjecttable = new SubjectRecord();
+                            subjecttable.SubjectID = "" + reader.GetInt32(0);
+                            subjecttable.Name = reader.GetString(1);
+                            subjecttable.DateCreated = reader.GetDateTime(2);
+
+                            listSubjects.Add(subjecttable);
+                        }
+                    }
+                }
+            }
+            return listSubjects;
         }
+    }
+
+
+
+    public class SubjectRecord
+    {
+        public string SubjectID;
+        public string Name;
+        public DateTime DateCreated;
 
     }
 }
