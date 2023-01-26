a1446fa314c2dd8a1557fdd1c09af523c77a1f43
Rida-Abid <107047216+Rida-Abid@users.noreply.github.com>
2023-01-26 20:06:40 +0500

    Connected database table class to classcontroller and Views/Class/Index.

--- Controllers/ClassController.cs
+++ Controllers/ClassController.cs
@@ -1,14 +1,32 @@
 ﻿using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using System.Data.SqlClient;
 
 namespace TTMS.Controllers
 {
+    //public class ClassController : Controller
+    //{
+    //    [Authorize]
+    //    public IActionResult Index()
+    //    {
+    //        return View();
+    //    }
+
+    //    [Authorize]
+    //    public IActionResult AddEdit()
+    //    {
+    //        return View();
+    //    }
+
+
+    //}
     public class ClassController : Controller
     {
         [Authorize]
         public IActionResult Index()
         {
-            return View();
+
+            return View(GetClasses());
         }
 
         [Authorize]
@@ -16,7 +34,44 @@ namespace TTMS.Controllers
         {
             return View();
         }
+        public List<ClassRecord> GetClasses()
+        {
 
+            string connectionString = "Data Source=.\\sqlexpress;Initial Catalog=tms;Integrated Security=True";
+            var listClasses = new List<ClassRecord>();
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                connection.Open();
+                string sql = "SELECT * FROM Class";
+                using (SqlCommand command = new SqlCommand(sql, connection))
+                {
+
+                    using (SqlDataReader reader = command.ExecuteReader())
+                    {
+                        while (reader.Read())
+                        {
+                            ClassRecord classtable = new ClassRecord();
+                            classtable.ClassID = "" + reader.GetInt32(0);
+                            classtable.Name = reader.GetString(1);
+                            classtable.DateCreated = reader.GetDateTime(2);
+
+                            listClasses.Add(classtable);
+                        }
+                    }
+                }
+            }
+            return listClasses;
+        }
+    }
+
+
+
+    public class ClassRecord
+    {
+        public string ClassID;
+        public string Name;
+        public DateTime DateCreated;
 
     }
 }
+
