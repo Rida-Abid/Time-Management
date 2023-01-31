4c009e9d4b5c710d55be7d2c70c30f88ddd2726f
Rida-Abid <107047216+Rida-Abid@users.noreply.github.com>
2023-01-31 19:47:25 +0500

    Installed Dapper into the project and changed delete and insert operations the dapper way.

--- Controllers/TeacherController.cs
+++ Controllers/TeacherController.cs
@@ -1,13 +1,23 @@
 ﻿using Dapper;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using System.Data;
 using System.Data.SqlClient;
+using static TTMS.Controllers.TeacherController;
 
 namespace TTMS.Controllers
 {
     public class TeacherController : Controller
     {
         private string ConnectionString = "Data Source=.\\sqlexpress;Initial Catalog=tms;Integrated Security=True";
+
+        public IDbConnection Connection
+        {
+            get
+            {
+                return new SqlConnection(ConnectionString);
+            }
+        }
         [Authorize]
         public IActionResult Index()
         {
@@ -16,122 +26,51 @@ namespace TTMS.Controllers
         }
 
         [Authorize]
-        public IActionResult Delete(TeacherRecord teacherRecord)
+        public IActionResult Delete(int id)
         {
-            DeleteTeacher(teacherRecord);
+            DeleteTeacher(id);
             return RedirectToAction("Index");
 
         }
-
-        private int DeleteTeacher(TeacherRecord teacherRecord)
+        private void DeleteTeacher(int id)
         {
-            using (SqlConnection connection = new SqlConnection(ConnectionString))
+            using (IDbConnection dbConnection = Connection)
             {
-                connection.Open();
-                var deletedRows = connection.Execute(@"Delete from Teacher Where TeacherID = @Id", new { Id = teacherRecord.TeacherID });
-                connection.Close();
-                return deletedRows;
+                string sql = @"DELETE FROM Teacher WHERE TeacherID = @Id";
+                dbConnection.Open();
+                dbConnection.Execute(sql, new { Id = id });
             }
-            //string connectionString ="Data Source=.\\sqlexpress;Initial Catalog=tms;Integrated Security=True";
-            //using (SqlConnection connection = new SqlConnection(connectionString))
-            //{
-            //    string sql = "DELETE FROM TEACHER WHERE TeacherID = @Id";
-            //    SqlCommand command = new SqlCommand(sql , connection);
-            //    SqlParameter paramId = new SqlParameter();
-            //    paramId.ParameterName = "@Id";
-            //    paramId.Value = id;
-            //    command.Parameters.Add(paramId);
-            //    connection.Open();
-            //    command.ExecuteNonQuery();
-            //}
+            
         }
 
         [Authorize]
-        public IActionResult Add()
-        {
-            return View();
-        }
-        [Authorize]
-        [HttpPost]
         public IActionResult Add(TeacherRecord teacherRecord)
         {
-            
             AddTeacher(teacherRecord);
             return View();
         }
-
-
-        private int AddTeacher(TeacherRecord teacherRecord)
+        private void AddTeacher(TeacherRecord teacherRecord)
         {
 
-            using (var connection = new SqlConnection(ConnectionString))
+            using (IDbConnection dbConnection = Connection)
             {
-                connection.Open();
-                var affectedRows = connection.Execute("INSERT INTO Teacher(Firstname, Surname, Email) Values(@Firstname, @Surname, @Email)", new {Firstname = teacherRecord.Firstname, teacherRecord.Surname, teacherRecord.Email });
-                connection.Close();
-                return affectedRows;
+                string sql = $"INSERT INTO Teacher(Title, Firstname, Surname, Email) VALUES('{teacherRecord.Title}', '{teacherRecord.Firstname}', '{teacherRecord.Surname}', '{teacherRecord.Email}')";
+                dbConnection.Open();
+                dbConnection.Execute(sql, teacherRecord);
             }
-            //    string connectionString = "Data Source=.\\sqlexpress;Initial Catalog=tms;Integrated Security=True";
-            //    using (SqlConnection connection = new SqlConnection(connectionString))
-            //    {
-            //        connection.Open();
-            //        string sql = "INSERT INTO Teacher(Title, Firstname, Surname, Email) Values(@Title, @Firstname, @Surname, @Email)";
-
-            //        SqlCommand command = new SqlCommand(sql, connection);
-            //        command.Parameters.Add(new SqlParameter { ParameterName = "@Title", Value = teacherRecord.Title });
-            //        command.Parameters.Add(new SqlParameter { ParameterName = "@Firstname", Value = teacherRecord.Firstname });
-            //        command.Parameters.Add(new SqlParameter { ParameterName = "@Surname", Value = teacherRecord.Surname });
-            //        command.Parameters.Add(new SqlParameter { ParameterName = "@Email", Value = teacherRecord.Email });
-
-
-            //        command.ExecuteNonQuery();
-            //        connection.Close();
-
-
-            //    }
+            
 
         }
-        public List<TeacherRecord> GetTeachers()
+        public IEnumerable<TeacherRecord> GetTeachers()
         {
-                List<TeacherRecord> teachers = new List<TeacherRecord>();
-                using (var connection = new SqlConnection(ConnectionString))
-                {
-                    connection.Open();
-                    teachers = connection.Query<TeacherRecord>("Select * from Teacher").ToList();
-                    connection.Close();
-                }
-                return teachers;
+            using (IDbConnection dbConnection = Connection)
+            {
+                string sql = @"SELECT * FROM Teacher";
+                dbConnection.Open();
+                return dbConnection.Query<TeacherRecord>(sql);
+            }
         }
 
-
-        //string connectionString = "Data Source=.\\sqlexpress;Initial Catalog=tms;Integrated Security=True";
-        //var listTeachers = new List<TeacherRecord>();
-        //using (SqlConnection connection = new SqlConnection(connectionString))
-        //{
-        //    connection.Open();
-        //    string sql = "SELECT * FROM Teacher";
-        //    using (SqlCommand command = new SqlCommand(sql, connection))
-        //    {
-
-        //        using (SqlDataReader reader = command.ExecuteReader())
-        //        {
-        //            while (reader.Read())
-        //            {
-        //                TeacherRecord teachertable = new TeacherRecord();
-        //                teachertable.TeacherID =  reader.GetInt32(0);
-        //                teachertable.Title = reader.GetString(1);
-        //                teachertable.Firstname = reader.GetString(2);
-        //                teachertable.Surname = reader.GetString(3);
-        //                teachertable.Email = reader.GetString(4);
-        //                teachertable.DateCreated = reader.GetDateTime(5);
-
-        //                listTeachers.Add(teachertable);
-        //            }
-
-        //        }
-        //    }
-        //}
-        //return listTeachers;
     
 
         public class TeacherRecord
