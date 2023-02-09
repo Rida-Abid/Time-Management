90f95a59682a4a636557518decdbbea241868bf9
Rida-Abid <107047216+Rida-Abid@users.noreply.github.com>
2023-02-09 16:34:59 +0500

    Removed Subject Column from TeacherTable

--- Scripts/TeacherTable.sql
+++ Scripts/TeacherTable.sql
@@ -6,17 +6,16 @@ Create table dbo.Teacher
 	Title varchar(5) NOT NULL,
 	Firstname varchar(50) NOT NULL,
 	Surname varchar(50) NOT NULL,
-	Subject varchar(50) NOT NULL,
 	Email varchar(50) NOT NULL,
 	CreatedDate DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
 	UpdatedDate DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP
 );
 
-INSERT INTO dbo.Teacher (Title, Firstname, Surname, Subject, Email)
+INSERT INTO dbo.Teacher (Title, Firstname, Surname, Email)
 VALUES 
-( 'Ms','Sidra','Maqsood','English','Sidra123@gmail.com'),
-( 'Ms','Iqra','Aftab','Urdu','IqraAftab@gmail.com'),
-( 'Ms','Laiba','Saqib','Mathematics','Laibasaqib@gmail.com'),
-( 'Mr','Ahmad','Jamal','Science','Ahmad256@gmail.com'),
-( 'Ms','Sadia','Nawaz','Islamiat','Sadia@gmail.com')
+( 'Ms','Sidra','Maqsood','Sidra123@gmail.com'),
+( 'Ms','Iqra','Aftab','IqraAftab@gmail.com'),
+( 'Ms','Laiba','Saqib','Laibasaqib@gmail.com'),
+( 'Mr','Ahmad','Jamal','Ahmad256@gmail.com'),
+( 'Ms','Sadia','Nawaz','Sadia@gmail.com')
  
