ed1ab28b9e71bf59ef31588495abaaf8bfcc89a5
Rida-Abid <107047216+Rida-Abid@users.noreply.github.com>
2023-02-17 20:27:23 +0500

    Added lookup Controller

---
+++ Controllers/API/LookupController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace TTMS.Controllers.API
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class LookupController : ControllerBase
+    {
+        private readonly DataController db;
+        public LookupController()
+        {
+            db = new DataController();
+        }
+
+        //[HttpGet]
+        //public JsonResult Get(int Id)
+        //{
+        //    db.GetSubjectsByTeacherId(Id);
+        //    return
+        //}
+        
+        
+
+        
+    }
+}
--- Scripts/TimeTable.sql
+++ Scripts/TimeTable.sql
@@ -1,9 +1,9 @@
 use [tms]
-CREATE TABLE dbo.TimeTable
+CREATE TABLE dbo.Timetable
 (   
 
      TimetableID int NOT NULL PRIMARY KEY IDENTITY(1,1), 
-     Name varchar(15) NOT NULL,
+     Name varchar(50) NOT NULL,
      TeacherID int NOT NULL,
      SubjectID int NOT NULL,
      ClassID int NOT NULL,
@@ -12,17 +12,17 @@ CREATE TABLE dbo.TimeTable
 
 );
     
-ALTER TABLE dbo.TimeTable WITH CHECK ADD CONSTRAINT FK_TimeTable_Teacher Foreign Key(TeacherID)
+ALTER TABLE dbo.Timetable WITH CHECK ADD CONSTRAINT FK_Timetable_Teacher Foreign Key(TeacherID)
 References dbo.Teacher (TeacherID)
 
-ALTER TABLE dbo.TimeTable WITH CHECK ADD CONSTRAINT FK_TimeTable_Subject Foreign Key(SubjectID)
+ALTER TABLE dbo.Timetable WITH CHECK ADD CONSTRAINT FK_Timetable_Subject Foreign Key(SubjectID)
 References dbo.Subject (SubjectID)
 
-ALTER TABLE dbo.TimeTable WITH CHECK ADD CONSTRAINT FK_TimeTable_Class Foreign Key(ClassID)
+ALTER TABLE dbo.Timetable WITH CHECK ADD CONSTRAINT FK_Timetable_Class Foreign Key(ClassID)
 References dbo.Class (ClassID)
 
-ALTER TABLE dbo.TimeTable WITH CHECK ADD CONSTRAINT FK_TimeTable_Lesson Foreign Key(LessonID)
+ALTER TABLE dbo.Timetable WITH CHECK ADD CONSTRAINT FK_Timetable_Lesson Foreign Key(LessonID)
 References dbo.Lessons (LessonID)
 
-ALTER TABLE dbo.TimeTable WITH CHECK ADD CONSTRAINT FK_TimeTable_Day Foreign Key(LessonID)
+ALTER TABLE dbo.Timetable WITH CHECK ADD CONSTRAINT FK_Timetable_Day Foreign Key(LessonID)
 References dbo.Days (DayID)
